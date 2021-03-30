5fa361f6a75b6209abea17bf227e2c564f8b1c09
haxorsprogramming <haxorsuinsu@gmail.com>
2021-03-30 19:39:46 +0700

    update

---
+++ Python for Data Professional/Task/2.py
@@ -0,0 +1,10 @@
+# Statment
+print("Belajar Python menyenangkan")
+print("Halo Dunia")
+print("Hello World!")
+# Variables & Literals
+bilangan1 = 5
+bilangan2 = 10
+kalimat1 = "Belajar Bahasa Python"
+# Operators 
+print(bilangan1 + bilangan2)
